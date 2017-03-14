c5e28e99f501a3108921f81594d8b1a4f98a342f
DuthpelaPavan <pavannzb25@gmail.com>
2017-03-14 06:17:03 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,63 +8,6 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var articles = {
-articleone : {
- title: 'artcleone',
- heading: 'article-one',
- date: 'date',
- body: 'hey their this is excellent'
-},
-articletwo : {
- title: 'artcletwo',
- heading: 'article-two',
- date: 'date',
- body: 'hey their this is excellent'
-},
-articlethree : {
- title: 'artclethree',
- heading: 'article-three',
- date: 'date',
- body: 'hey their this is excellent'
-}
-};
-function createTemplate(data){
-    var title=data.title;
-    var heading=data.heading;
-    var date=data.date;
-    var body=data.body;
-var htmlTemplate = `
-<html>
-    <head>
-        <title>
-            ${title}
-        </title>
-    <link href="/ui/style.css" rel="stylesheet">
-   </head>
-    <body>
-        <div class="pavan">
-        <div>
-            <h2>${heading}</h2>
-        </div>
-        <div>
-          ${date}
-        </div>
-        <hr/>
-        <div>
-            ${body}
-        </div>
-        </div>
-    </body>
-</html>
-`;
-return htmlTemplate;
-}
-
-app.get('/:articleName', function (req, res) {
-    // /:articleName = articleone
-    var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
