ceeec21ce5635b48464881576c3cf321a96f86e1
DuthpelaPavan <pavannzb25@gmail.com>
2017-03-14 05:36:28 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+
+var img=document.getElementById('img');
+
+var marginLeft=0;
+function moveRight(){
+    marginLeft = marginLeft+2;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
+
+img.onclick = function(){
+    var interval = setInterval(moveRight,200);
+};
+
