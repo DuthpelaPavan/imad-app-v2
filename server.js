d485de56e586b4d22406e4e14d6c1add48ce6ff7
DuthpelaPavan <pavannzb25@gmail.com>
2017-03-14 06:20:20 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-
+var counter=0;
 app.get('/counterplus', function (req, res) {
-    var counter=counter+1;
+     counter=counter+1;
   res.send(counter.toString());
 });
 
