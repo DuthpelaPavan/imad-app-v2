f149439aa82e2c57685fa1724d9277e443195744
DuthpelaPavan <pavannzb25@gmail.com>
2017-03-14 06:53:06 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,19 @@ img.onclick = function(){
 var counter=0;
 var button=document.getElementById('counter');
 button.onclick=function(){
-var span=document.getElementById('count');
+    var request= new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readystate === XMLHttpRequest.DONE){
+            if(request.readystatus === 200){
+                var span=document.getElementById('count');
 counter=counter+1;
 span.innerHTML=counter.toString();
+            }
+        }
+        
+    };
+    request.open('GET','http://duthpelapavan.imad.hasura-app.io/counterplus',true);
+    request.send(null);
+    
+
 };
