628478a4d4518bb8f31f1c4174d82c7b177454e8
DuthpelaPavan <pavannzb25@gmail.com>
2017-03-14 07:01:13 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,8 +17,8 @@ var button=document.getElementById('counter');
 button.onclick=function(){
     var request= new XMLHttpRequest();
     request.onreadystatechange=function(){
-        if(request.readystate === XMLHttpRequest.DONE){
-            if(request.readystatus === 200){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
                 var span=document.getElementById('count');
 counter=request.responseText;
 span.innerHTML=counter.toString();
